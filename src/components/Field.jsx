6ae6faa34c113237f992e032639b33449b1cc19c
Захаров Максим <juzehdhd@gmail.com>
2025-09-18 00:24:45 +0700

    Удалил лишнее состояние

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -27,9 +27,6 @@ function Field({reset}) {
     const [cords, setCords] = useState({x: 0, y: 0, rotation: 0})
     const [currentFigure, setCurrentFigure] = useState([[]])
     const [field, setField] = useState({blocks: [], score: 0})
-
-    const [figuresPlaced, setFiguresPlaced] = useState(0)
-
     const [leftPressed, rightPressed, upPressed, downPressed] = useKeysAction(downRef, upRef, leftRef, rightRef)
 
     const [pauseDown, pauseManualDown, pauseManualLeft, pauseManualRight] = useTimerAction(75, updateCords, [
@@ -57,7 +54,7 @@ function Field({reset}) {
             pause: true,
             launchOnCount: 1 // Должно быть единицей, чтобы нормально работало смещение в сторону перед заверешением
         },
-    ], stop, [figuresPlaced])
+    ], stop)
 
     function updateCords(offset) {
         setCords(prev => {
